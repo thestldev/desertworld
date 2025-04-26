7bc7e1600fd40c6aaa9e39a42435678396ca8c70
DVSh (Dozya) <dozer747474@gmail.com>
2025-04-27 03:49:01 +0500

    Частично сделано торнадо

--- src/main/java/com/idlenonsense/desertworld/entity/SandstormEntity.java
+++ src/main/java/com/idlenonsense/desertworld/entity/SandstormEntity.java
@@ -1,84 +1,64 @@
 package com.idlenonsense.desertworld.entity;
 
-import net.fabricmc.loader.impl.game.minecraft.Hooks;
-import net.minecraft.client.util.NetworkUtils;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.mob.ZombieEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Arm;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-
 import java.util.ArrayList;
-import java.util.List;
 
-@Deprecated
-public class SandstormEntity extends PathAwareEntity {
+public class SandstormEntity extends LivingEntity {
     private int age = 0;
-    public float size = 6.0F;
-    public float yLevel = 5.0F;
-    public LivingEntity owner;
-    public Vec3d velocity = new Vec3d(0.0D, 0.0D, 0.0D);
+    private float size = 6.0F;
+    private float yLevel = 5.0F;
+    private LivingEntity owner;
+    private Vec3d velocity = new Vec3d(0.0D, 0.0D, 0.0D);
 
     private final Iterable<ItemStack> armor = new ArrayList<>();
     private final ItemStack currentStack = new ItemStack(ItemStack.EMPTY.getItem());
 
-    public SandstormEntity(EntityType<? extends PathAwareEntity> type, World world) {
+    public SandstormEntity(EntityType<? extends LivingEntity> type, World world) {
         super(type, world);
+        this.age = 0;
+        this.size = 6.0F;
+        this.yLevel = 5.0F;
+        this.owner = null;
+    }
+
+    @Override
+    protected void initDataTracker() {
+
     }
 
     public static DefaultAttributeContainer.Builder createSandstormAttributes() {
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 10000.0D)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.0D)
-                ;
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.0D);
     }
 
     @Override
     public void tick() {
         ++this.age;
         if (world instanceof ServerWorld serverWorld) {
-            List<Entity> entities;
             if (this.age > 300) {
                 this.discard();
-                entities = serverWorld.getOtherEntities(this.owner, new Box(this.getPos().x, this.getPos().y, this.getPos().z, this.getPos().x + 5, this.getPos().y + 5, this.getPos().z + 5));
-                entities.forEach((entity) -> {
-                    entity.setNoGravity(false);
-                });
                 return;
             }
 
             if (this.age % 3 == 0) {
                 for (int i = 0; (double) i < (double) this.size * 1.4D; ++i) {
-                    this.spawnParticleRing((float) i / 1.4F, (float) i / 6.0F, Math.max(1, i / 4) * 2, serverWorld);
+                    spawnTornadoParticles(serverWorld, (float) i / 1.4F, (float) i / 6.0F);
                 }
             }
-
-            if (this.age % 20 == 0) {
-                this.yLevel = Math.min(this.yLevel + (float) this.random.nextBetween(-2, 2), 3.0F);
-                this.size = this.size * 0.8F;
-            }
-
-            this.setYaw(this.getYaw() - 3.0F);
-            entities = serverWorld.getOtherEntities(this, new Box(this.getBlockPos()).expand(this.size), (entity) -> {
-                return entity instanceof PathAwareEntity && !(entity instanceof PlayerEntity);
-            });
-            entities.forEach(this::computeEntity);
-            this.setVelocity(this.velocity);
         }
     }
 
@@ -87,56 +67,19 @@ public class SandstormEntity extends PathAwareEntity {
         return null;
     }
 
-    public void spawnParticleRing(float y, float radius, int count, ServerWorld serverWorld) {
-        for (int i = 0; i < count; ++i) {
-            float radians = (float) i / (float) count * 6.2831855F + this.getYaw() * 3.1415927F / 180.0F;
-            double cos = Math.cos(radians) * radius;
-            double sin = Math.sin(radians) * radius;
-            if (Math.random() > 0.04D) {
-                serverWorld.getPlayers().forEach((serverPlayer) -> {
-                    serverWorld.spawnParticles(serverPlayer, ParticleTypes.FLASH, true, this.getX() + (double) cos, this.getY() + (double) y, this.getZ() + (double) sin, 1, 0.4D, 0.4D, 0.4D, 0.0D);
-                });
-            } else {
-                serverWorld.getPlayers().forEach((serverPlayer) -> {
-                    serverWorld.spawnParticles(serverPlayer, ParticleTypes.FIREWORK, true, this.getX() + (double) cos, this.getY() + (double) y, this.getZ() + (double) sin, 1, 0.4D, 0.4D, 0.4D, 0.0D);
-                });
-            }
-        }
-    }
-
-    public void computeEntity(Entity entity) {
-        Vec3d towards = this.getPos().add(0.0D, (double) this.yLevel, 0.0D).subtract(entity.getPos()).normalize();
-        Vec3d move = towards.rotateY(75.0F);
-        entity.setNoGravity(true);
-        entity.setVelocity(move.multiply(0.5D, 0.5D, 0.5D));
-        double distance = entity.getPos().distanceTo(this.getPos().add(0.0D, (double) this.yLevel, 0.0D));
-        if (distance < 25.0D) {
-            if (this.owner == null) {
-                entity.damage(DamageSource.MAGIC, 2.0F);
-            } else {
-                entity.damage(DamageSource.mob(this.owner), 2.0F);
-            }
-        }
-    }
-
     @Override
-    protected void initDataTracker() {
-    }
+    public void readCustomDataFromNbt(NbtCompound nbt) {
 
-    @Override
-    public void readCustomDataFromNbt(NbtCompound tag) {
-        this.size = tag.getFloat("Size");
-        this.age = tag.getInt("Ticks");
     }
 
     @Override
     public Iterable<ItemStack> getArmorItems() {
-        return armor;
+        return null;
     }
 
     @Override
     public ItemStack getEquippedStack(EquipmentSlot slot) {
-        return currentStack;
+        return null;
     }
 
     @Override
@@ -145,8 +88,26 @@ public class SandstormEntity extends PathAwareEntity {
     }
 
     @Override
-    public void writeCustomDataToNbt(NbtCompound tag) {
-        tag.putFloat("Size", this.size);
-        tag.putInt("Ticks", this.age);
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+
+    }
+
+    @Override
+    public Packet<?> createSpawnPacket() {
+        return null;
+    }
+
+    private void spawnTornadoParticles(ServerWorld world, float y, float radius) {
+        int count = 20;
+        for (int i = 0; i < count; ++i) {
+            float angle = (float) i / (float) count * 6.2831855F + this.getYaw() * 3.1415927F / 180.0F;
+            double cos = Math.cos(angle) * radius;
+            double sin = Math.sin(angle) * radius;
+            world.spawnParticles(ParticleTypes.WHITE_ASH,
+                    this.getX() + cos,
+                    this.getY() + y,
+                    this.getZ() + sin,
+                    1, 0.4D, 0.4D, 0.4D, 0.0D);
+        }
     }
 }
--- src/main/java/com/idlenonsense/desertworld/item/SandstormItem.java
+++ src/main/java/com/idlenonsense/desertworld/item/SandstormItem.java
@@ -1,18 +1,25 @@
 package com.idlenonsense.desertworld.item;
 
-import com.idlenonsense.desertworld.entity.ModEntities;
-import com.idlenonsense.desertworld.entity.SandstormEntity;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class SandstormItem extends Item {
+    private static final int PARTICLE_SPAWN_INTERVAL = 20;
+    private static final int TOTAL_TICKS = 120;
+
+    private int ticksElapsed = 0;
+
     public SandstormItem(Settings settings) {
         super(settings);
     }
@@ -20,23 +27,48 @@ public class SandstormItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
-        world.playSound(
-                null,
-                user.getX(),
-                user.getY(),
-                user.getZ(),
-                SoundEvents.BLOCK_SAND_PLACE,
-                SoundCategory.NEUTRAL,
-                0.5F,
-                0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F)
-        );
         if (!world.isClient) {
-            SandstormEntity sandstormEntity = new SandstormEntity(ModEntities.SANDSTORM_ENTITY, world);
-            world.spawnEntity(sandstormEntity);
+            ticksElapsed = 0;
+            BlockPos userPos = user.getBlockPos();
+            startSandstorm(world, userPos, user);
         }
-
         user.incrementStat(Stats.USED.getOrCreateStat(this));
-
         return TypedActionResult.success(itemStack, world.isClient());
+    }
+
+    private void startSandstorm(World world, BlockPos pos, PlayerEntity user) {
+        if (world instanceof ServerWorld serverWorld) {
+            spawnParticles(serverWorld, pos);
+            ServerTickEvents.START_SERVER_TICK.register(server -> {
+                if (ticksElapsed < TOTAL_TICKS) {
+                    if (ticksElapsed % PARTICLE_SPAWN_INTERVAL == 0) {
+                        spawnParticles(serverWorld, pos);
+                    }
+                    ticksElapsed++;
+                }
+            });
+        }
+        world.playSound(null, pos, SoundEvents.BLOCK_SAND_PLACE, SoundCategory.NEUTRAL, 0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+    }
+
+    private void spawnParticles(ServerWorld world, BlockPos pos) {
+        int radius = 5;
+        int count = 200;
+        int layers = 7;
+        double step = radius / (double) layers;
+        for (int j = 0; j < layers; j++) {
+            double currentRadius = radius - step * j;
+            for (int i = 0; i < count; ++i) {
+                float angle = (float) i / count * 6.2831855F;
+                double cos = Math.cos(angle) * currentRadius;
+                double sin = Math.sin(angle) * currentRadius;
+                double height = pos.getY() + 7.5D - (j * 1.0D);
+                world.spawnParticles(ParticleTypes.CLOUD,
+                        pos.getX() + cos,
+                        height,
+                        pos.getZ() + sin,
+                        1, 0.4D, 0.4D, 0.4D, 0.0D);
+            }
+        }
     }
 }
